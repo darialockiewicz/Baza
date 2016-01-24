e5345266176206ba806d5378015b879354774bda
darialockiewicz <vardruna666@gmail.com>
2016-01-24 22:59:32 +0100

    Update and rename init to init.py

--- init.py
+++ init.py
@@ -10,15 +10,15 @@ c.execute('''
           CREATE TABLE Bankaccountoperations
           ( id INTEGER PRIMARY KEY,
             op_date DATE NOT NULL,
-			      balance NUMERIC NOT NULL
+            balance NUMERIC NOT NULL
           )
           ''')
 c.execute('''
           CREATE TABLE Bankaccountoperationsitems
           ( title VARCHAR(30),
             balance NUMERIC NOT NULL,
-			      in_out INTEGER NOT NULL,
-			      bankaccountoperations_id INTEGER,
-			      FOREIGN KEY(bankaccountoperations_id) REFERENCES Bankaccountoperations(id),
+            in_out INTEGER NOT NULL,
+            bankaccountoperations_id INTEGER,
+            FOREIGN KEY(bankaccountoperations_id) REFERENCES Bankaccountoperations(id),
             PRIMARY KEY (bankaccountoperations_id))
           ''')
