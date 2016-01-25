2000effa828376aeb58ab279c5ce049585de8e0d
darialockiewicz <vardruna666@gmail.com>
2016-01-25 21:39:22 +0100

    init.py

---
+++ init.py
@@ -0,0 +1,24 @@
+import sqlite3
+
+
+db_path = 'bankaccountoperations.db'
+conn = sqlite3.connect(db_path)
+
+c = conn.cursor()
+
+c.execute('''
+          CREATE TABLE Bankaccountoperations
+          ( id INTEGER PRIMARY KEY,
+            op_date DATE NOT NULL,
+			      balance NUMERIC NOT NULL
+          )
+          ''')
+c.execute('''
+          CREATE TABLE Bankaccountoperationsitems
+          ( title VARCHAR(30),
+            balance NUMERIC NOT NULL,
+			      in_out INTEGER NOT NULL,
+			      bankaccountoperations_id INTEGER,
+			      FOREIGN KEY(bankaccountoperations_id) REFERENCES Bankaccountoperations(id),
+            PRIMARY KEY (title, bankaccountoperations_id))
+          ''')
